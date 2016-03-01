94af99ea2045ed2d8f0b700870dbb09ab652acea
liqiang <274981091@qq.com>
2016-03-01 22:53:40 +0800

    修改了邀请

--- src/com/moblong/amuse/RequestInvitation.java
+++ src/com/moblong/amuse/RequestInvitation.java
@@ -15,10 +15,11 @@ import org.springframework.web.context.support.WebApplicationContextUtils;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.moblong.amuse.utils.RabbitConnectionPool;
-import com.moblong.flipped.model.Message;
+import com.moblong.flipped.model.Whistle;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
 
 @SuppressWarnings("serial")
 @WebServlet(displayName="RequestInvitation", name ="RequestInvitation", urlPatterns = "/RequestInvitation")
@@ -30,20 +31,22 @@ public final class RequestInvitation extends HttpServlet {
 		Gson gson = new GsonBuilder()
 				    .setDateFormat("yyyy-MM-dd HH:mm:ss")
 				    .create();
-		Message message = null;
+		Whistle message = null;
 		if(msg != null)
-			message = gson.fromJson(msg, Message.class);
+			message = gson.fromJson(msg, Whistle.class);
 		
 		WebApplicationContext context = WebApplicationContextUtils.getWebApplicationContext(getServletContext());
 		
 		Connection con;
 	    Channel channel;
-	    RabbitConnectionPool pool = context.getBean("RabbitConnectionPool", RabbitConnectionPool.class);
-		con = pool.getConnection();
-		channel = con.createChannel();
-	    channel.queueDeclare(message.getTarget(), false, false, false, null);
-	    channel.basicPublish("", message.getTarget(), null, msg.getBytes("UTF-8"));
+	    //RabbitConnectionPool pool = context.getBean("RabbitConnectionPool", RabbitConnectionPool.class);
+	    final ConnectionFactory factory = new ConnectionFactory();
+	    factory.setHost("push.tlthsc.com");
 	    try {
+	    	con = factory.newConnection();
+			channel = con.createChannel();
+		    channel.queueDeclare(message.getTarget(), false, false, false, null);
+		    channel.basicPublish("", message.getTarget(), null, msg.getBytes("UTF-8"));
 			channel.close();
 			channel = null;
 		} catch (TimeoutException e) {
