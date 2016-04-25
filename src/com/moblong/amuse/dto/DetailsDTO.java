d31a79d5deb250866fa8645af17143cdbc55d5ea
liqiang <274981091@qq.com>
2016-04-25 22:12:01 +0800

    去掉了t_details表，修改了相应的DetailsDTO

--- src/com/moblong/amuse/dto/DetailsDTO.java
+++ src/com/moblong/amuse/dto/DetailsDTO.java
@@ -41,7 +41,7 @@ public final class DetailsDTO {
 		try {
 			con = ds.getConnection();
 			con.setAutoCommit(false);
-			pstat = con.prepareStatement("SELECT COUNT(1) FROM t_details_base WHERE uid = ?");
+			pstat = con.prepareStatement("SELECT COUNT(1) FROM t_contact_indicator WHERE uid = ?");
 			pstat.setString(1, uid);
 			pstat.execute();
 			rs = pstat.getResultSet();
@@ -54,22 +54,32 @@ public final class DetailsDTO {
 			pstat = null;
 			
 			if(exist) {
-				pstat = con.prepareStatement("DELETE FROM t_details_base WHERE uid = ?");
+				pstat = con.prepareStatement("DELETE FROM t_contact_indicator WHERE uid = ?");
 				pstat.setString(1, uid);
 				pstat.execute();
 				pstat.close();
 				pstat = null;
 			}
 			
-			pstat = con.prepareStatement("INSERT INTO t_details_base(uid, iid, title, content, condition) VALUES(?, ?, ?, ?, ?)");
+			/**
+			 * uid character(128),
+			   iid integer,
+			   title character varying(32),
+			   content text,
+			   modifyed date DEFAULT now(),
+			   visible boolean NOT NULL DEFAULT false,
+			   credibility boolean NOT NULL DEFAULT false 
+			 */
+			pstat = con.prepareStatement("INSERT INTO t_user_indicator(uid, iid, title, content, modifyed, visible, credibility) VALUES(?, ?, ?, ?, ?, ?, ?)");
 			for(VerifiableItem item : details) {
 				Indicator<String> indicator = item.getIndicator();
 				pstat.setString(1, uid);
-				pstat.setInt(2,    indicator.getId());
+				pstat.setInt(2, indicator.getId());
 				pstat.setString(3, indicator.getTitle());
 				pstat.setString(4, indicator.getContent());
-				pstat.setInt(5,    indicator.isVisible() ? 1 : 0);
-				pstat.setInt(6,    item.isVerified() ? 1 : 0);
+				pstat.setDate(5, new java.sql.Date(System.currentTimeMillis()));
+				pstat.setBoolean(6, indicator.isVisible());
+				pstat.setBoolean(7, item.isVerified());
 				pstat.addBatch();
 			}
 			pstat.executeBatch();
@@ -110,13 +120,13 @@ public final class DetailsDTO {
 		try {
 			user = new ArrayList<VerifiableItem>(10);
 			con = ds.getConnection();
-			pstat = con.prepareStatement("SELECT uid, iid, title, content, condition FROM t_details_base WHERE uid = ?");
+			pstat = con.prepareStatement("SELECT uid, iid, title, content, modifyed, visible, credibility FROM t_contact_indicator WHERE uid = ?");
 			pstat.setString(1, uid);
 			pstat.execute();
 			rs = pstat.getResultSet();
 			while(rs.next()) {
 				int iid = rs.getInt("iid");
-				user.add(new VerifiableItem(uid, new Indicator<String>(iid, rs.getString("title"), rs.getString("content")), rs.getInt("condition") > 0));
+				user.add(new VerifiableItem(uid, new Indicator<String>(iid, rs.getString("title"), rs.getString("content")), rs.getBoolean("credibility")));
 			}
 			rs.close();
 			rs = null;
@@ -164,7 +174,7 @@ public final class DetailsDTO {
 			DataSource ds = context.getBean("ds", DataSource.class);
 			con = ds.getConnection();
 			con.setAutoCommit(false);
-			pstat = con.prepareStatement("DELETE FROM t_user_indicator WHERE uid = ?");
+			pstat = con.prepareStatement("DELETE FROM t_contact_indicator WHERE uid = ?");
 			pstat.setString(1, uid);
 			pstat.execute();
 			con.commit();
@@ -193,59 +203,5 @@ public final class DetailsDTO {
 				con = null;
 			}
 		}
-	}
-	
-	public String lookfor(final ApplicationContext context, final String aid) {
-		String				uid = null;
-		Connection			con = null;
-		PreparedStatement pstat = null;
-		ResultSet			 rs = null;
-		DataSource 			 ds = context.getBean("ds", DataSource.class);
-		try {
-			con = ds.getConnection();
-			pstat = con.prepareStatement("SELECT uid FROM t_account_base WHERE aid = ?");
-			pstat.setString(1, aid);
-			pstat.execute();
-			rs = pstat.getResultSet();
-			if(rs.next()) {
-				uid = rs.getString(1);
-			}
-			rs.close();
-			rs = null;
-			pstat.close();
-			pstat = null;
-			con.close();
-			con = null;
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if(rs != null) {
-				try {
-					rs.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				rs = null;
-			}
-			
-			if(pstat != null) {
-				try {
-					pstat.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				pstat = null;
-			}
-			
-			if(con != null) {
-				try {
-					con.close();
-				} catch (SQLException e) {
-					e.printStackTrace();
-				}
-				con = null;
-			}
-		}
-		return uid;
 	}
 }
